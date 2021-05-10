99d6c170cf58242456d11370cfc01ab2fef96e03
Nawshin Rahman <71029312+nrahma06@users.noreply.github.com>
2021-05-10 19:45:31 -0400

    Created Quiz and Results pages

---
+++ quiz.php
@@ -0,0 +1,78 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Final Project Quiz Page</title>
+    <link type="text/css" rel="stylesheet" href="quiz.css">
+</head>
+<body>
+
+    <h1>"Guess The Meme" Quiz! </h1>
+    <h2>Nawshin Rahman, Destiny Stribling, Nuran Ghoneim </h2>
+    <h3>ITEC 305 M01 - Internet Programming I</h3>
+    <h4>Professor Salayka</h4>
+    <h5>May 20th, 2021</h5>
+
+    <hr />
+    <p>Instructions: Welcome to "Guess The Meme" Quiz! This quiz consists of 10 questions with 4 options. Out of the 4 choices,
+    only one of them is the correct answer. Choose wisely. Have fun!
+    </p>
+
+    <ol>
+        <li> Testing <br /> <label>
+            <input type = "radio" name = "guess" value = "guess1" /> <br />
+            <input type = "radio" name = "guess" value = "guess2" /> <br />
+            <input type = "radio" name = "guess" value = "guess3" /> <br />
+            <input type = "radio" name = "guess" value = "guess4" /> </label> </li>
+        <li> Testing <br /> <label>
+            <input type = "radio" name = "guess" value = "guess1" /> <br />
+            <input type = "radio" name = "guess" value = "guess2" /> <br />
+            <input type = "radio" name = "guess" value = "guess3" /> <br />
+            <input type = "radio" name = "guess" value = "guess4" /> </label> </li>
+        <li> Testing <br /> <label>
+            <input type = "radio" name = "guess" value = "guess1" /> <br />
+            <input type = "radio" name = "guess" value = "guess2" /> <br />
+            <input type = "radio" name = "guess" value = "guess3" /> <br />
+            <input type = "radio" name = "guess" value = "guess4" /> </label> </li>
+        <li> Testing <br /> <label>
+            <input type = "radio" name = "guess" value = "guess1" /> <br />
+            <input type = "radio" name = "guess" value = "guess2" /> <br />
+            <input type = "radio" name = "guess" value = "guess3" /> <br />
+            <input type = "radio" name = "guess" value = "guess4" /> </label> </li>
+        <li> Testing <br /> <label>
+            <input type = "radio" name = "guess" value = "guess1" /> <br />
+            <input type = "radio" name = "guess" value = "guess2" /> <br />
+            <input type = "radio" name = "guess" value = "guess3" /> <br />
+            <input type = "radio" name = "guess" value = "guess4" /> </label> </li>
+        <li> Testing <br /> <label>
+            <input type = "radio" name = "guess" value = "guess1" /> <br />
+            <input type = "radio" name = "guess" value = "guess2" /> <br />
+            <input type = "radio" name = "guess" value = "guess3" /> <br />
+            <input type = "radio" name = "guess" value = "guess4" /> </label> </li>
+        <li> Testing <br /> <label>
+            <input type = "radio" name = "guess" value = "guess1" /> <br />
+            <input type = "radio" name = "guess" value = "guess2" /> <br />
+            <input type = "radio" name = "guess" value = "guess3" /> <br />
+            <input type = "radio" name = "guess" value = "guess4" /> </label> </li>
+        <li> Testing <br /> <label>
+            <input type = "radio" name = "guess" value = "guess1" /> <br />
+            <input type = "radio" name = "guess" value = "guess2" /> <br />
+            <input type = "radio" name = "guess" value = "guess3" /> <br />
+            <input type = "radio" name = "guess" value = "guess4" /> </label> </li>
+        <li> Testing <br /> <label>
+            <input type = "radio" name = "guess" value = "guess1" /> <br />
+            <input type = "radio" name = "guess" value = "guess2" /> <br />
+            <input type = "radio" name = "guess" value = "guess3" /> <br />
+            <input type = "radio" name = "guess" value = "guess4" /> </label> </li>
+        <li> Testing <br /> <label>
+            <input type = "radio" name = "guess" value = "guess1" /> <br />
+            <input type = "radio" name = "guess" value = "guess2" /> <br />
+            <input type = "radio" name = "guess" value = "guess3" /> <br />
+            <input type = "radio" name = "guess" value = "guess4" /> </label> </li>
+    </ol>
+    
+</body>
+</html>
