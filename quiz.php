d7cd363457919bea41741ca5153acbc5107c64b2
Nawshin Rahman <71029312+nrahma06@users.noreply.github.com>
2021-05-12 18:32:43 -0400

    Added the starting questions on the Quiz page and the score header on the results page. Adjusted the style on the results css page

--- quiz.php
+++ quiz.php
@@ -21,52 +21,52 @@
     </p>
 
     <ol>
-        <li> Complete the meme: ________. <br /> <label>
+        <li> Complete the meme: Keep Calm and _____. <br /> <label>
             <input type = "radio" name = "guess1" value = "guess1" /> <br />
             <input type = "radio" name = "guess2" value = "guess2" /> <br />
             <input type = "radio" name = "guess3" value = "guess3" /> <br />
             <input type = "radio" name = "guess4" value = "guess4" /> </label> </li>
-        <li> Complete the meme: ________. <br /> <label>
+        <li> Complete the meme: Real question is what________. <br /> <label>
             <input type = "radio" name = "guess5" value = "guess5" /> <br />
             <input type = "radio" name = "guess6" value = "guess6" /> <br />
             <input type = "radio" name = "guess7" value = "guess7" /> <br />
             <input type = "radio" name = "guess8" value = "guess8" /> </label> </li>
-        <li> Complete the meme: ________. <br /> <label>
+        <li> Complete the meme: I just came to have a good time________. <br /> <label>
             <input type = "radio" name = "guess9" value = "guess9" /> <br />
             <input type = "radio" name = "guess10" value = "guess10" /> <br />
             <input type = "radio" name = "guess11" value = "guess11" /> <br />
             <input type = "radio" name = "guess12" value = "guess12" /> </label> </li>
-        <li> Complete the meme: ________. <br /> <label>
+        <li> Complete the meme: Oh my god, I'm gonna cry! My diamond earring!________. <br /> <label>
             <input type = "radio" name = "guess13" value = "guess13" /> <br />
             <input type = "radio" name = "guess14" value = "guess14" /> <br />
             <input type = "radio" name = "guess15" value = "guess15" /> <br />
             <input type = "radio" name = "guess16" value = "guess16" /> </label> </li>
-        <li> Complete the meme: ________. <br /> <label>
+        <li> Complete the meme: Look at________. <br /> <label>
             <input type = "radio" name = "guess17" value = "guess17" /> <br />
             <input type = "radio" name = "guess18" value = "guess18" /> <br />
             <input type = "radio" name = "guess19" value = "guess19" /> <br />
             <input type = "radio" name = "guess20" value = "guess20" /> </label> </li>
-        <li> Complete the meme: ________. <br /> <label>
+        <li> Complete the meme: You did it________. <br /> <label>
             <input type = "radio" name = "guess21" value = "guess21" /> <br />
             <input type = "radio" name = "guess22" value = "guess22" /> <br />
             <input type = "radio" name = "guess23" value = "guess23" /> <br />
             <input type = "radio" name = "guess24" value = "guess24" /> </label> </li>
-        <li> Complete the meme: ________. <br /> <label>
+        <li> Complete the meme: Oh hi thanks for checking in________. <br /> <label>
             <input type = "radio" name = "guess25" value = "guess25" /> <br />
             <input type = "radio" name = "guess26" value = "guess26" /> <br />
             <input type = "radio" name = "guess27" value = "guess27" /> <br />
             <input type = "radio" name = "guess28" value = "guess28" /> </label> </li>
-        <li> Complete the meme: ________. <br /> <label>
+        <li> Complete the meme: Damn Daniel________. <br /> <label>
             <input type = "radio" name = "guess29" value = "guess29" /> <br />
             <input type = "radio" name = "guess30" value = "guess30" /> <br />
             <input type = "radio" name = "guess31" value = "guess31" /> <br />
             <input type = "radio" name = "guess32" value = "guess32" /> </label> </li>
-        <li> Complete the meme: ________. <br /> <label>
+        <li> Complete the meme: I don't need friends________. <br /> <label>
             <input type = "radio" name = "guess33" value = "guess33" /> <br />
             <input type = "radio" name = "guess34" value = "guess34" /> <br />
             <input type = "radio" name = "guess35" value = "guess35" /> <br />
             <input type = "radio" name = "guess36" value = "guess36" /> </label> </li>
-        <li> Complete the meme: ________. <br /> <label>
+        <li> Complete the meme: He needs________. <br /> <label>
             <input type = "radio" name = "guess37" value = "guess37" /> <br />
             <input type = "radio" name = "guess38" value = "guess38" /> <br />
             <input type = "radio" name = "gues39" value = "guess39" /> <br />
--- results.php
+++ results.php
@@ -11,62 +11,45 @@
 </head>
 <body>
 
-    <h1>Do You Know Your Memes? </h1>
+    <h1>Do You Know Your Memes </h1>
+    <img src="https://media.tenor.com/images/d55a83e9abb508b54415a8672e0da24c/tenor.gif" alt="Head Bopping Cat" class="headericon">
+    <img src="https://media.tenor.com/images/d55a83e9abb508b54415a8672e0da24c/tenor.gif" alt="Head Bopping Cat" class="headericon2">
     <h2>Nawshin Rahman, Destiny Stribling, Nuran Ghoneim </h2>
 
     <hr />
+    <p> Score: </p>
     
     <ol>
-        <li> Testing <br /> <label>
-            <input type = "radio" name = "guess" value = "guess1" /> <br />
-            <input type = "radio" name = "guess" value = "guess2" /> <br />
-            <input type = "radio" name = "guess" value = "guess3" /> <br />
-            <input type = "radio" name = "guess" value = "guess4" /> </label> </li>
-        <li> Testing <br /> <label>
-            <input type = "radio" name = "guess" value = "guess1" /> <br />
-            <input type = "radio" name = "guess" value = "guess2" /> <br />
-            <input type = "radio" name = "guess" value = "guess3" /> <br />
-            <input type = "radio" name = "guess" value = "guess4" /> </label> </li>
-        <li> Testing <br /> <label>
-            <input type = "radio" name = "guess" value = "guess1" /> <br />
-            <input type = "radio" name = "guess" value = "guess2" /> <br />
-            <input type = "radio" name = "guess" value = "guess3" /> <br />
-            <input type = "radio" name = "guess" value = "guess4" /> </label> </li>
-        <li> Testing <br /> <label>
-            <input type = "radio" name = "guess" value = "guess1" /> <br />
-            <input type = "radio" name = "guess" value = "guess2" /> <br />
-            <input type = "radio" name = "guess" value = "guess3" /> <br />
-            <input type = "radio" name = "guess" value = "guess4" /> </label> </li>
-        <li> Testing <br /> <label>
-            <input type = "radio" name = "guess" value = "guess1" /> <br />
-            <input type = "radio" name = "guess" value = "guess2" /> <br />
-            <input type = "radio" name = "guess" value = "guess3" /> <br />
-            <input type = "radio" name = "guess" value = "guess4" /> </label> </li>
-        <li> Testing <br /> <label>
-            <input type = "radio" name = "guess" value = "guess1" /> <br />
-            <input type = "radio" name = "guess" value = "guess2" /> <br />
-            <input type = "radio" name = "guess" value = "guess3" /> <br />
-            <input type = "radio" name = "guess" value = "guess4" /> </label> </li>
-        <li> Testing <br /> <label>
-            <input type = "radio" name = "guess" value = "guess1" /> <br />
-            <input type = "radio" name = "guess" value = "guess2" /> <br />
-            <input type = "radio" name = "guess" value = "guess3" /> <br />
-            <input type = "radio" name = "guess" value = "guess4" /> </label> </li>
-        <li> Testing <br /> <label>
-            <input type = "radio" name = "guess" value = "guess1" /> <br />
-            <input type = "radio" name = "guess" value = "guess2" /> <br />
-            <input type = "radio" name = "guess" value = "guess3" /> <br />
-            <input type = "radio" name = "guess" value = "guess4" /> </label> </li>
-        <li> Testing <br /> <label>
-            <input type = "radio" name = "guess" value = "guess1" /> <br />
-            <input type = "radio" name = "guess" value = "guess2" /> <br />
-            <input type = "radio" name = "guess" value = "guess3" /> <br />
-            <input type = "radio" name = "guess" value = "guess4" /> </label> </li>
-        <li> Testing <br /> <label>
-            <input type = "radio" name = "guess" value = "guess1" /> <br />
-            <input type = "radio" name = "guess" value = "guess2" /> <br />
-            <input type = "radio" name = "guess" value = "guess3" /> <br />
-            <input type = "radio" name = "guess" value = "guess4" /> </label> </li>
+        <li> Your Answer: 
+        <p> </p>
+        </li>
+        <li> Your Answer: 
+        <p> </p>
+        </li>
+        <li> Your Answer:  
+        <p> </p>
+        </li>
+        <li> Your Answer:  
+        <p> </p>
+        </li>
+        <li> Your Answer:  
+        <p> </p>
+        </li>
+        <li> Your Answer: 
+        <p> </p>
+        </li>
+        <li> Your Answer:  
+        <p> </p>
+        </li>
+        <li> Your Answer: 
+        <p> </p>
+        </li>
+        <li> Your Answer:  
+        <p> </p>
+        </li>
+        <li> Your Answer: 
+        <p> </p>
+        </li>
     </ol>
 
     <form action= quiz.php >
